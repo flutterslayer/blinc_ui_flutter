522dbbb08ca575832bc54cb1f44a6a7a9d23e3fa
Antônio Teixeira <antonio.teixeira@blinctek.com>
2022-09-22 09:02:12 -0300

    feat: add spacing class

---
+++ lib/src/spacing.dart
@@ -0,0 +1,15 @@
+class Spacing {
+  static const _baseValue = 8;
+
+  static const zero = 0; // 0
+  static const xxxs = _baseValue; // 8
+  static const xxs = _baseValue * 2; // 16
+  static const xs = _baseValue * 3; // 24
+  static const sm = _baseValue * 4; // 32
+  static const md = _baseValue * 5; // 40
+  static const lg = _baseValue * 6; // 48
+  static const xl = _baseValue * 7; // 56
+  static const xxl = _baseValue * 8; // 64
+  static const xxxl = _baseValue * 9; // 72
+  static const huge = _baseValue * 10; // 80
+}
