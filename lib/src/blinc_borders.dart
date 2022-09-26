890b6b34c9434042a785f727a6e1ed0bef5cd11d
Rodolfo1705 <roabrahao5@gmail.com>
2022-09-26 15:53:35 -0300

    feat: adding reference comments

--- lib/src/blinc_borders.dart
+++ lib/src/blinc_borders.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 
+/// Creates pre defined styles for [Container] borders.
+/// It can be used by the border argument at [BoxDecoration].
+/// For example:
+/// ```dart
+///Container(
+///  decoration: BoxDecoration(
+///    border: BlincBorders.md,
+///  ),
+///);
+/// ```
+
 class BlincBorders {
+  ///The default blinc border [color]
   static const color = Color(0xff858583);
 
+  ///The [zero] size provides a non-border [Container]
   static BoxBorder zero = Border.all(
     width: 0,
   );
 
+  ///The [sm] border size measures 0.5px
   static BoxBorder sm = Border.all(
     color: color,
     width: 0.5,
   );
 
+  ///The [md] border size measures 1px
   static BoxBorder md = Border.all(
     color: color,
     width: 1,
   );
 
+  ///The [lg] border size measures 2px
   static BoxBorder lg = Border.all(
     color: color,
     width: 2,
