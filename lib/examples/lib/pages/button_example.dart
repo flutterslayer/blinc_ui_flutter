b3bcf8ded237e9b61108a24c5ba908360ee19b6a
Antônio Teixeira <antonio.teixeira@blinctek.com>
2022-09-27 10:21:45 -0300

    feat: add button examples

---
+++ lib/examples/lib/pages/button_example.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:blinc_ui_flutter/src/button_components.dart';
+
+class ButtonExample extends StatelessWidget {
+  const ButtonExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        title: const Text('Button component example'),
+      ),
+      body: SafeArea(
+        child: SizedBox(
+            child: Column(
+          children: [
+            const BlincButton(
+              text: 'SMALL PRIMAdasddasdsasdRY',
+            ).smallPrimary(),
+            const BlincButton(
+              text: 'SMALL SECONDARY',
+            ).smallSecondary(),
+            const BlincButton(
+              text: 'SMALL TERTIARY',
+            ).smallTertiary(),
+            const BlincButton(
+              text: 'LARGE PRIMARY',
+            ).largePrimary(),
+            const BlincButton(
+              text: 'LARGE SECONDARY',
+            ).largeSecondary(),
+            const BlincButton(
+              text: 'LARGE TERTIARY',
+            ).largeTertiary(),
+          ],
+        )),
+      ),
+    );
+  }
+}
--- lib/examples/lib/pages/select_screen.dart
+++ lib/examples/lib/pages/select_screen.dart
@@ -19,18 +19,27 @@ class SelectScreen extends StatelessWidget {
           mainAxisSpacing: 10,
           crossAxisCount: 3,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/spacing');
-              },
-              child: const Text(
-                'Spacing',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
+            selectBlock(context: context, route: '/spacing', text: 'Spacing'),
+            selectBlock(context: context, route: '/buttons', text: 'Buttons'),
           ],
         )),
       ),
     );
   }
+}
+
+Widget selectBlock({
+  required BuildContext context,
+  required String route,
+  required String text,
+}) {
+  return ElevatedButton(
+    onPressed: () {
+      Navigator.pushNamed(context, route);
+    },
+    child: Text(
+      text,
+      style: const TextStyle(fontSize: 16),
+    ),
+  );
 }
