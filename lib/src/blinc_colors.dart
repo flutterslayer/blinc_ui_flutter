64c66427a3de948921b6bbacd40be2d8a6ed103c
marianneblincteker <103218626+marianneblincteker@users.noreply.github.com>
2022-09-28 13:37:25 -0300

    feat: blinc colors package

---
+++ lib/src/blinc_colors.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors();
+
+  /// Neutrals
+  /// Titles
+  static const Color colorNeutral_900 = Color(0xFF1A1A19);
+
+  /// Primary button bg, Icons, Texts
+  static const Color colorNeutral_800 = Color(0xFF434342);
+
+  /// Bigger texts (≥ 18pt)
+  static const Color colorNeutral_600 = Color(0xFF858583);
+
+  static const Color colorNeutral_400 = Color(0xFFB2B2B1);
+
+  /// Secondary button bg
+  static const Color colorNeutral_200 = Color(0xFFDCDCDA);
+
+  static const Color colorNeutral_100 = Color(0xFFE3E3E1);
+
+  /// Background
+  static const Color colorNeutral_50 = Color(0xFFF2F2EF);
+
+  static const Color colorNeutral_0 = Color(0xFFFFFFFF);
+
+  ///Brand Blues
+  static const Color colorBluePrimary = Color(0xFF2364AE);
+  static const Color colorBlueSecondary = Color(0xFF044E81);
+
+  ///Brand Greens
+  static const Color colorGreenPrimary = Color(0xFF02B1A8);
+  static const Color colorGreenSecondary = Color(0xFF008985);
+
+  ///Brand Oranges
+  static const Color colorOrangePrimary = Color(0xFFF4BA18);
+  static const Color colorOrangeSecondary = Color(0xFFE28D25);
+
+  ///Feedback Colors
+  /// Error messages. Can be used with red-50, neutral-0 or neutral-50.
+  static const Color colorRedError_300 = Color(0xFFD10644);
+
+  ///  Error messages for darker backgrounds, icons and strokes. Can be used with neutral-900.
+  static const Color colorRedError_200 = Color(0xFFFA627D);
+
+  ///  Error bg.
+  static const Color colorRedErrorBackground_50 = Color(0xFFFFF2F6);
+
+  /// Success messages. Can be used with green-50, neutral-0 or neutral-50.
+  static const Color colorGreenSuccess_300 = Color(0xFF0E7A53);
+
+  /// Success messages for darker backgrounds, icons and strokes. Can be used with neutral-900, neutral-800.
+  static const Color colorGreenSuccess_200 = Color(0xFF36C88A);
+
+  /// Success bg.
+  static const Color colorGreenSuccessBackground_50 = Color(0xFFF2F8F5);
+
+  /// Alert messages. Can be used with orange-50, neutral-0 or neutral-50.
+  static const Color colorOrangeAlert_300 = Color(0xFFBA4A34);
+
+  /// Alert messages for darker backgrounds, icons and strokes. Can be used with neutral-900, neutral-800.
+  static const Color colorOrangeAlert_200 = Color(0xFFFAA71A);
+
+  /// Alert bg.
+  static const Color colorOrangeAlertBackground_50 = Color(0xFFFEF7E2);
+
+  /// Informative messages. Can be used with blue-50, neutral-0 or neutral-50.
+  static const Color colorBlueInfo_300 = Color(0xFF0063D8);
+
+  /// Informative messages for darker backgrounds, icons and strokes. Can be used with neutral-900.
+  static const Color colorBlueInfo_200 = Color(0xFF4B96ED);
+
+  /// Informative bg.
+  static const Color colorBlueInfoBackground_50 = Color(0xFFEEF5FD);
+}
