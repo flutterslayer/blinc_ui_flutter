156534e02af1e0e995cbe0c70e673fe0e83d7cd5
Fábio Pacheco <fabio.pacheco@blinctek.com>
2022-10-13 11:02:51 -0300

    Chore: finish inputs and add intl

--- lib/src/blinc_input_component.dart
+++ lib/src/blinc_input_component.dart
@@ -1,5 +1,6 @@
 import 'package:blinc_ui_flutter/blinc_ui_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class BlincInputComponent {
   static Widget textField({
@@ -108,6 +109,36 @@ class _BlincInputTextFieldState extends State<BlincInputTextField> {
     super.dispose();
   }
 
+  Color _borderStyleRule() {
+    if (_errorMessage != null) {
+      return AppColors.colorRedError_300;
+    }
+
+    return _borderColor;
+  }
+
+  Widget _iconSytleRule(
+    IconData? icon, {
+    bool suffixIcon = false,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 10,
+        right: 10,
+      ),
+      child: icon != null
+          ? Icon(
+              suffixIcon == true && _errorMessage != null
+                  ? Icons.new_releases_outlined
+                  : icon,
+              color: (suffixIcon == true && _errorMessage != null
+                  ? AppColors.colorRedError_300
+                  : AppColors.colorNeutral_800),
+            )
+          : null,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -116,7 +147,9 @@ class _BlincInputTextFieldState extends State<BlincInputTextField> {
         Container(
           decoration: BoxDecoration(
             color: widget.enabled == false ? AppColors.colorNeutral_50 : null,
-            border: Border.all(color: _borderColor),
+            border: Border.all(
+              color: _borderStyleRule(),
+            ),
             borderRadius: BorderRadius.circular(8),
           ),
           child: TextFormField(
@@ -128,14 +161,18 @@ class _BlincInputTextFieldState extends State<BlincInputTextField> {
               if (widget.validator == null) {
                 return null;
               }
-
               setState(() {
                 _errorMessage = widget.validator!(value);
               });
-
-              return null;
+              return _errorMessage;
             },
             decoration: InputDecoration(
+              errorMaxLines: 1,
+              errorStyle: const TextStyle(
+                color: Colors.transparent,
+                fontSize: 0,
+                height: -10,
+              ),
               contentPadding: const EdgeInsets.symmetric(vertical: 24),
               border: InputBorder.none,
               labelText: widget.label,
@@ -144,29 +181,10 @@ class _BlincInputTextFieldState extends State<BlincInputTextField> {
               floatingLabelBehavior: widget.placeholder != null
                   ? FloatingLabelBehavior.always
                   : null,
-              prefixIcon: Padding(
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  right: 10,
-                ),
-                child: widget.prefixIcon != null
-                    ? Icon(
-                        widget.prefixIcon,
-                        color: AppColors.colorNeutral_800,
-                      )
-                    : null,
-              ),
-              suffixIcon: Padding(
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  right: 10,
-                ),
-                child: widget.suffixIcon != null
-                    ? Icon(
-                        widget.suffixIcon,
-                        color: AppColors.colorNeutral_800,
-                      )
-                    : null,
+              prefixIcon: _iconSytleRule(widget.prefixIcon),
+              suffixIcon: _iconSytleRule(
+                widget.suffixIcon,
+                suffixIcon: true,
               ),
               labelStyle: TextStyle(
                 color: _labelColor,
@@ -226,5 +244,91 @@ class BlincInputValidation {
       return errorText;
     }
     return null;
+  }
+
+  String? validatePasscode(String? value) {
+    if (value == null) {
+      return 'Passcode required';
+    }
+    if (value.isNotEmpty) {
+      if (value.length < 8) {
+        return 'Passcode must have 8 or more characters';
+      }
+
+      if (RegExp('[A-Z]').hasMatch(value) == false) {
+        return 'Passcode must have an uppercase character';
+      }
+
+      for (var i = 0; i < value.length - 2; i++) {
+        try {
+          final first = int.tryParse(value[i]);
+          final second = int.tryParse(value[i + 1]);
+          final third = int.tryParse(value[i + 2]);
+
+          if (first != null && second != null && third != null) {
+            if (first == second && first == third) {
+              return 'Passcode cannot contain 3 or more equal numbers';
+            }
+
+            if (first == second - 1 && first == third - 2) {
+              return 'Passcode cannot contain sequence of 3 or more numbers';
+            }
+          }
+        } catch (e) {
+          return 'Invalid Password. Please review it.';
+        }
+      }
+    }
+    return null;
+  }
+
+  String? validateDateOfBirth(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'You need to informe a date.';
+    }
+    try {
+      final data = DateFormat('M/d/y').parseStrict(value);
+      final now = DateTime.now();
+      if (data.isAfter(DateTime(now.year - 18, now.month, now.day))) {
+        return 'You must be 18 or older to create an\n account.';
+      }
+      if (data.isBefore(DateTime(1900))) {
+        return 'Please enter a valid date.';
+      }
+    } catch (e) {
+      return 'Please enter a date in mm/dd/yyyy format.';
+    }
+    return null;
+  }
+
+  String? validateName(String? value) {
+    if (value == null || value.length < 3) {
+      return 'Enter your Name';
+    }
+    return null;
+  }
+
+  String? validateCellPhone(String? value) {
+    if (value == null || value.length < 14) {
+      return 'Please enter a valid a phone number';
+    }
+    return null;
+  }
+
+  String? validateMail(String? value) {
+    final valid = RegExp(
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
+    ).hasMatch(value ?? '');
+
+    if (!valid) {
+      return 'Please enter a valid e-mail';
+    }
+    return null;
+  }
+
+  bool isValidEmail(String value) {
+    return RegExp(
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
+    ).hasMatch(value);
   }
 }
