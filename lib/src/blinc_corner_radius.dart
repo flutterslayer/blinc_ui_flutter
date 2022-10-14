848802a27f70c9ca776ba848ef98238f5110df52
Rodolfo1705 <roabrahao5@gmail.com>
2022-10-14 12:05:52 -0300

    chore: changing corner doc text

--- lib/src/blinc_corner_radius.dart
+++ lib/src/blinc_corner_radius.dart
@@ -15,14 +15,15 @@ class BlincCornerRadius {
   ///The [sharp] size provides a non-corner [Container]
   static BorderRadiusGeometry sharp = BorderRadius.circular(0);
 
-  ///The [sm] border size measures 0.5px
+  ///The [sm] corner radius size renders a [Container] that has all radii with circular(8px)
   static BorderRadiusGeometry sm = BorderRadius.circular(8);
 
-  ///The [md] border size measures 1px
+  ///The [md] corner radius size renders a [Container] that has all radii with  circular(16px)
   static BorderRadiusGeometry md = BorderRadius.circular(16);
 
-  ///The [lg] border size measures 2px
+  ///The [lg] corner radius size renders a [Container] that has all radii with  circular(24px)
   static BorderRadiusGeometry lg = BorderRadius.circular(24);
 
+  ///The [circle] corner radius renders a [Container] that is a circle
   static BorderRadiusGeometry circle = BorderRadius.circular(9999);
 }
