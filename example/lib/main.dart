77933db67c18747bd3c3120ad32744fef3c37b44
Antônio Teixeira <antonio.teixeira@blinctek.com>
2022-11-08 15:46:39 -0400

    feat: add example to main page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-
+import 'package:blinc_ui_flutter/blinc_ui_flutter.dart';
 import 'package:examples/pages/borders_example.dart';
 import 'package:examples/pages/brand_example.dart';
 import 'package:examples/pages/button_example.dart';
@@ -24,7 +24,78 @@ void main() {
         '/brand': (context) => const BrandExample(),
         '/progress_bar': (context) => const ProgressBarExample(),
         '/text': (context) => const TextExample(),
+        '/login': (context) => const LoginPage(),
       },
     ),
   );
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final AppBar _appBar = AppBar(
+    elevation: 0,
+    backgroundColor: Colors.white,
+    foregroundColor: Colors.red,
+  );
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar,
+      backgroundColor: Colors.white,
+      body: LayoutBuilder(
+        builder: (context, constraints) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: BlincInputComponent.form(
+            globalKey: _formKey,
+            child: ListView(
+              children: [
+                const Spacer(
+                  flex: 2,
+                ),
+                BlincText('Login').weightLight.heightHuge.sizeHuge,
+                BlincSpacer.vertical.xxs,
+                BlincText(
+                        'Welcome back! Enter your email and password to continue.')
+                    .weightRegular
+                    .heightSM
+                    .sizeSM,
+                BlincSpacer.vertical.xs,
+                BlincInputComponent.textField(
+                  textEditingController: _emailController,
+                  label: 'Email',
+                  validator: BlincInputComponent.validations.validateMail,
+                ),
+                BlincSpacer.vertical.xxxs,
+                BlincInputComponent.textField(
+                  textEditingController: _passController,
+                  label: 'Password',
+                  validator: BlincInputComponent.validations.validatePasscode,
+                ),
+                BlincSpacer.vertical.xxxs,
+                BlincButton(
+                  text: 'LOGIN',
+                  onPressed: () {
+                    if (!_formKey.currentState!.validate()) {
+                      return;
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
